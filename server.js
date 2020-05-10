6cce343adf42ca89564e33403311dfef8badbf8a
viktor <lvigtor@gmail.com>
2020-05-11 02:28:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 
+const port = process.env.PORT || 5000;
+
 app = express();
 app.use(serveStatic(__dirname));
 
-const port = process.env.PORT || 5000;
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
 
 app.listen(port);
 
